6c59587f949bcba9b10997396403970d17116b19
E. Alejandro Pardo-Sarmiento <165843464+PardoEdgar@users.noreply.github.com>
2026-07-25 10:11:50 -0500

    Update 02_HRV_metrics_extraction.R

--- scripts/02_HRV_metrics_extraction.R
+++ scripts/02_HRV_metrics_extraction.R
@@ -1,4 +1,8 @@
-all_valleys <- read.csv("all_valleys_data.csv")
+library(readxl)
+library(RHRV)
+all_valleys <- readxl::read_xlsx("C:/Users/jandr/OneDrive - Universidad del rosario/Temperature_JP_HRV_data/Data/data_extraction/S1_File_Supplementary_data_Pardo_Sarmiento.xlsx", sheet = "all_valleys")
+all_valleys$Time <- as.numeric(all_valleys$Time)
+all_valleys$Value <- as.numeric(all_valleys$Value)
 
 extract_HRV_metrics <- function(hrv_obj, id) {
   if (!is.null(hrv_obj$TimeAnalysis[[1]])) {
@@ -15,7 +19,7 @@ extract_HRV_metrics <- function(hrv_obj, id) {
     )
   } else {
     data.frame(
-      ID = id,
+      ID = ID,
       MeanHR = NA,
       SDNN = NA,
       RMSSD = NA,
@@ -25,13 +29,12 @@ extract_HRV_metrics <- function(hrv_obj, id) {
     )
   }
 }
-
 grouped <- all_valleys %>% group_by(Treatment, ID)
 data_by_snail <- grouped %>% group_split()
 Real_IDs <- grouped %>% group_keys()
 results_HRV<- lapply(data_by_snail, function(df_snail) {
   HRV <- CreateHRVData()
-  HRV <- LoadBeatVector(HRV, df_snail$time)
+  HRV <- LoadBeatVector(HRV, df_snail$Time)
   HRV <- BuildNIHR(HRV)
   HRV <- InterpolateNIHR(HRV)
   HRV <- CreateTimeAnalysis(HRV, size = 60, interval = 7.8125)
@@ -45,3 +48,46 @@ HRV_metrics_total<- imap_dfr(results_HRV, ~ extract_HRV_metrics(.x, .y))
 HRV_metrics_total <- HRV_metrics_total %>%
   tidyr::separate(ID, into = c("Treatment", "ID"), sep = "_")
 
+View(HRV_metrics_total)
+
+
+
+
+extract_RR <- function(hrv_obj, ID) {
+  if (!is.null(hrv_obj$TimeAnalysis[[1]])) {
+    ta <- hrv_obj$TimeAnalysis[[1]]  
+    RR_ms <- hrv_obj$Beat
+    data.frame(
+      ID = ID,
+      RRms= (RR_ms$RR),
+      stringsAsFactors = FALSE
+    )
+  } else {
+    data.frame(
+      ID = ID,
+      RRms= NA,
+      stringsAsFactors = FALSE
+    )
+  }
+}
+
+RR_data <- imap_dfr(results_HRV, ~ extract_RR(.x, .y))
+
+RR_data <- RR_data %>%
+  tidyr::separate(ID, into = c("Treatment", "ID"), sep = "_")
+
+pNN100_function <- function(rr, x = 100) {
+  diff_rr <- abs(diff(rr))   # Succesive differences
+  nnx <- sum(diff_rr > x)    # Number of differences > 100 ms
+  pnnx <- nnx / length(diff_rr) * 100
+  return(pnnx)
+}
+
+pnn100_data <- RR_data %>%
+  group_by(ID) %>%
+  summarise(pNN100= pNN100_function(RRms, x = 100)) 
+
+View(pnn100_data)
+
+HRV_metrics_total <- HRV_metrics_total %>%
+  left_join(pnn100_data, by = "ID") 
