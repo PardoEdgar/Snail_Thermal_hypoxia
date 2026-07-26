61152c519972e6c8f6f47173f52e8d5215feb887
E. Alejandro Pardo-Sarmiento <165843464+PardoEdgar@users.noreply.github.com>
2026-07-26 16:10:55 -0500

    Refactor HRV metrics extraction script with tidyverse

--- scripts/02_HRV_metrics_extraction.R
+++ scripts/02_HRV_metrics_extraction.R
@@ -1,14 +1,16 @@
 library(readxl)
 library(RHRV)
+library(tidyverse)
+
 all_valleys <- readxl::read_xlsx("data.xlsx", sheet = "all_valleys")
 all_valleys$Time <- as.numeric(all_valleys$Time)
 all_valleys$Value <- as.numeric(all_valleys$Value)
 
 extract_HRV_metrics <- function(hrv_obj, ID) {
   if (!is.null(hrv_obj$TimeAnalysis[[1]])) {
-    ta <- hrv_obj$TimeAnalysis[[1]] 
-    RR_ms <- hrv_obj$Beat 
-      data.frame(
+    ta <- hrv_obj$TimeAnalysis[[1]]
+    RR_ms <- hrv_obj$Beat
+    data.frame(
       ID = ID,
       MeanHR = mean(hrv_obj$HR, na.rm = TRUE),
       SDNN = ta$SDNN,
@@ -32,7 +34,7 @@ extract_HRV_metrics <- function(hrv_obj, ID) {
 grouped <- all_valleys %>% group_by(Treatment, ID)
 data_by_snail <- grouped %>% group_split()
 Real_IDs <- grouped %>% group_keys()
-results_HRV<- lapply(data_by_snail, function(df_snail) {
+results_HRV <- lapply(data_by_snail, function(df_snail) {
   HRV <- CreateHRVData()
   HRV <- LoadBeatVector(HRV, df_snail$Time)
   HRV <- BuildNIHR(HRV)
@@ -41,9 +43,13 @@ results_HRV<- lapply(data_by_snail, function(df_snail) {
   return(HRV)
 })
 
-names(results_HRV) <- names(results_HRV) <- paste0(Real_IDs$Treatment, "_", Real_IDs$ID)
+names(results_HRV) <- names(results_HRV) <- paste0(
+  Real_IDs$Treatment,
+  "_",
+  Real_IDs$ID
+)
 
-HRV_metrics_total<- imap_dfr(results_HRV, ~ extract_HRV_metrics(.x, .y))
+HRV_metrics_total <- imap_dfr(results_HRV, ~ extract_HRV_metrics(.x, .y))
 
 HRV_metrics_total <- HRV_metrics_total %>%
   tidyr::separate(ID, into = c("Treatment", "ID"), sep = "_")
@@ -51,17 +57,17 @@ HRV_metrics_total <- HRV_metrics_total %>%
 
 extract_RR <- function(hrv_obj, ID) {
   if (!is.null(hrv_obj$TimeAnalysis[[1]])) {
-    ta <- hrv_obj$TimeAnalysis[[1]]  
+    ta <- hrv_obj$TimeAnalysis[[1]]
     RR_ms <- hrv_obj$Beat
     data.frame(
       ID = ID,
-      RRms= (RR_ms$RR),
+      RRms = (RR_ms$RR),
       stringsAsFactors = FALSE
     )
   } else {
     data.frame(
       ID = ID,
-      RRms= NA,
+      RRms = NA,
       stringsAsFactors = FALSE
     )
   }
@@ -73,32 +79,33 @@ RR_data <- RR_data %>%
   tidyr::separate(ID, into = c("Treatment", "ID"), sep = "_")
 
 pNN100_function <- function(rr, x = 100) {
-  diff_rr <- abs(diff(rr))   # Succesive differences
-  nnx <- sum(diff_rr > x)    # Number of differences > 100 ms
+  diff_rr <- abs(diff(rr)) # Succesive differences
+  nnx <- sum(diff_rr > x) # Number of differences > 100 ms
   pnnx <- nnx / length(diff_rr) * 100
   return(pnnx)
 }
 
-
 cv_function <- function(rr) {
-  mean <- mean(rr)   
-  sd <- sd(rr)   
-  CV <- sd / mean * 100
+  mean <- mean(rr)
+  sd <- sd(rr)
+  CV <- (sd / mean) * 100
   return(CV)
 }
 
+
 CV_data <- RR_data %>%
-  group_by(ID) %>%
-  summarise(CV = cv_function(RRms)) 
+  group_by(Treatment, ID) %>%
+  summarise(CV = cv_function(RRms), .groups = "drop")
 
 
 pnn100_data <- RR_data %>%
-  group_by(ID) %>%
-  summarise(pNN100= pNN100_function(RRms, x = 100)) 
+  group_by(Treatment, ID) %>%
+  summarise(pNN100 = pNN100_function(RRms, x = 100), .groups = "drop")
 
 HRV_metrics_total <- HRV_metrics_total %>%
-  left_join(pnn100_data, by = "ID")  %>%   left_join(CV_data, by = "ID")
-
+  left_join(pnn100_data, by = c("Treatment", "ID")) %>%
+  left_join(CV_data, by = c("Treatment", "ID"))
 
 view(HRV_metrics_total)
+
 
