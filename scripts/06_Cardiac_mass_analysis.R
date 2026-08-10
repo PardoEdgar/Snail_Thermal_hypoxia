512c693924fe267fd20cd2202fa158651791d736
E. Alejandro Pardo-Sarmiento <165843464+PardoEdgar@users.noreply.github.com>
2026-08-09 21:35:09 -0500

    Update 06_Cardiac_mass_analysis.R

--- scripts/06_Cardiac_mass_analysis.R
+++ scripts/06_Cardiac_mass_analysis.R
@@ -3,21 +3,18 @@
 # Author: Edgar Alejandro Pardo-Sarmiento
 ###############################################################################
 library(tidyverse)
-library(lme4)
-library(lmerTest)
 library(ggpubr)
 library(broom)
-library(ggpubr)
 library(readxl)
 library(car)
 
 body_mass <- readxl::read_xlsx(
-  "C:/Users/jandr/OneDrive - Universidad del rosario/Temperature_JP_HRV_data/Data/data_extraction/S1_File_Supplementary_data_Pardo_Sarmiento.xlsx",
+"C:/Users/jandr/OneDrive - Universidad del rosario/Temperature_JP_HRV_data/Pardo_Sarmiento _et_al_2/S1_File_Supplementary_data_Pardo_Sarmiento.xlsx",
   sheet = "Body_mass"
 )
 body_mass$ID <- as.character(body_mass$ID)
 HRV_metrics_total <- readxl::read_xlsx(
-  "C:/Users/jandr/OneDrive - Universidad del rosario/Temperature_JP_HRV_data/Data/data_extraction/S1_File_Supplementary_data_Pardo_Sarmiento.xlsx",
+"C:/Users/jandr/OneDrive - Universidad del rosario/Temperature_JP_HRV_data/Pardo_Sarmiento _et_al_2/S1_File_Supplementary_data_Pardo_Sarmiento.xlsx",
   sheet = "HRV_metrics_total"
 )
 
@@ -30,7 +27,6 @@ HRV_metrics_total <- HRV_metrics_total %>%
       "RMSSD",
       "pNN50",
       "NNi",
-      "pNN100",
       "CV"
     ),
     as.numeric
@@ -43,12 +39,12 @@ combined_data <- left_join(
   by = c("ID", "Treatment", "Block")
 ) %>%
   dplyr::filter(
-    Temperature %in% c(1, 8, 15, 21, 29, 36)
+    Temperature %in% c(1.5, 8, 15, 21, 29, 36)
   ) %>%
   dplyr::mutate(
     Temperature = factor(
       Temperature,
-      levels = c(1, 8, 15, 21, 29, 36)
+      levels = c(1.5, 8, 15, 21, 29, 36)
     )
   )
 combined_data$Temperature <- factor(combined_data$Temperature)
@@ -88,11 +84,6 @@ mass_HR <- ggplot(
   scale_x_continuous(limits = c(0, 8), expand = c(0, 0)) +
   theme_classic2()
 
-model_lm <- lm(
-  MeanHR ~ Temperature * `Mass_(g)`,
-  data = combined_data
-)
-summary(model_lmm)
 
 combined_data$Mass_centered <- combined_data$`Mass_(g)` - mean(combined_data$`Mass_(g)`, na.rm = TRUE)
 model_lm_centered <- lm(MeanHR ~ Temperature * Mass_centered, data = combined_data)
