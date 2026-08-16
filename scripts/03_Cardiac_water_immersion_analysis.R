131670c7d018ff82648144ecd265522a07f1322f
E. Alejandro Pardo-Sarmiento <165843464+PardoEdgar@users.noreply.github.com>
2026-08-16 17:52:59 -0500

    Update 03_Cardiac_water_immersion_analysis.R

--- scripts/03_Cardiac_water_immersion_analysis.R
+++ scripts/03_Cardiac_water_immersion_analysis.R
@@ -1,26 +1,38 @@
-
 ###############################################################################
 # Cardiac parameters analysis (Comparison before and after water immersion)
 # Author: Edgar Alejandro Pardo-Sarmiento
 ###############################################################################
-
 library(tidyverse)
 library(readxl)
 library(rstatix)
 library(ggpubr)
 
-#Results: HR & HRV baseline
-HRV_metrics_total <- readxl::read_xlsx("S1_File_Supplementary_data_Pardo_Sarmiento.xlsx", 
+body_mass <- readxl::read_xlsx(
+  "C:/Users/jandr/OneDrive - Universidad del rosario/Temperature_JP_HRV_data/Pardo_Sarmiento _et_al_2/S1_File_Supplementary_data_Pardo_Sarmiento.xlsx",
+  sheet = "Body_mass"
+)
+body_mass$ID <- as.character(body_mass$ID)
+HRV_metrics_total <- readxl::read_xlsx(
+  "C:/Users/jandr/OneDrive - Universidad del rosario/Temperature_JP_HRV_data/Pardo_Sarmiento _et_al_2/S1_File_Supplementary_data_Pardo_Sarmiento.xlsx",
   sheet = "HRV_metrics_total"
 )
 HRV_metrics_total <- HRV_metrics_total %>%
   mutate(across(
-    c(Temperature, MeanHR, SDNN, RMSSD, pNN50, NNi, CV),
+    c("Temperature", "MeanHR", "SDNN", "RMSSD", "pNN50", "NNi", "CV"),
     as.numeric
   ))
+HRV_metrics_total$ID <- as.character(HRV_metrics_total$ID)
 
+combined_data <- left_join(
+  HRV_metrics_total,
+  body_mass,
+  by = c("ID", "Treatment", "Block")
+)
+combined_data$Temperature <- factor(combined_data$Temperature)
 
-HRV_metrics_baseline <- HRV_metrics_total %>%
+#Results: HR & HRV baseline
+
+HRV_metrics_baseline <- combined_data %>%
   dplyr::filter(
     Treatment %in% c("ENVCOLD", "WATCOLD", "ENVHEAT", "WATHEAT")
   ) %>%
@@ -31,8 +43,7 @@ HRV_metrics_baseline <- HRV_metrics_total %>%
     )
   )
 
-
-HRV_metrics_baseline_cold <- HRV_metrics_total %>%
+HRV_metrics_baseline_cold <- combined_data %>%
   dplyr::filter(Treatment %in% c("ENVCOLD", "WATCOLD")) %>%
   dplyr::mutate(
     Treatment = factor(
@@ -41,7 +52,7 @@ HRV_metrics_baseline_cold <- HRV_metrics_total %>%
     )
   )
 
-HRV_metrics_baseline_heat <- HRV_metrics_total %>%
+HRV_metrics_baseline_heat <- combined_data %>%
   dplyr::filter(Treatment %in% c("ENVHEAT", "WATHEAT")) %>%
   dplyr::mutate(
     Treatment = factor(
@@ -50,38 +61,124 @@ HRV_metrics_baseline_heat <- HRV_metrics_total %>%
     )
   )
 
-n <- HRV_metrics_baseline %>% group_by(Treatment) %>% shapiro_test(MeanHR)
-n
-levene_test(HRV_metrics_baseline_heat, MeanHR ~ Treatment)
-t_test(HRV_metrics_baseline_heat, MeanHR ~ Treatment)
-levene_test(HRV_metrics_baseline_cold, MeanHR ~ Treatment)
-t_test(HRV_metrics_baseline_cold, MeanHR ~ Treatment)
 
-mean_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment) %>%
-  summarise(mean_HR = mean(MeanHR), sd_HR = sd(MeanHR))
-mean_baseline
-
-shapiro_test(HRV_metrics_baseline_heat$CV)
-n <- HRV_metrics_baseline %>% group_by(Treatment) %>% shapiro_test(CV)
-n 
-
-levene_test(HRV_metrics_baseline_cold, CV ~ Treatment)
-t_test(HRV_metrics_baseline_cold, CV ~ Treatment)
-
-levene_test(HRV_metrics_baseline_heat, CV ~ Treatment)
-wilcox_test(HRV_metrics_baseline_heat, CV ~ Treatment) #CV no normal para esta comparación
+paired_cold <- HRV_metrics_baseline_cold |>
+  select(`Mass_(g)`, Treatment, MeanHR) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = MeanHR
+  ) |>
+  drop_na(ENVCOLD, WATCOLD)
 
 
-mean_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment) %>% dplyr::filter(Treatment %in% c("ENVCOLD","WATCOLD"))%>% 
-  summarise(Median_CV = mean(CV), sd_CV = sd(CV))
-mean_baseline
+diff_cold <- paired_cold$WATCOLD - paired_cold$ENVCOLD
+shapiro.test(diff_cold)
 
-median_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment) %>% dplyr::filter(Treatment %in% c("ENVHEAT","WATHEAT"))%>% 
-  summarise(Median_CV = median(CV), IQR_CV = IQR(CV))
-median_baseline
+
+t.test(
+  paired_cold$ENVCOLD,
+  paired_cold$WATCOLD,
+  paired = TRUE
+)
+
+
+paired_heat <- HRV_metrics_baseline_heat |>
+  select(`Mass_(g)`, Treatment, MeanHR) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = MeanHR
+  ) |>
+  drop_na(ENVHEAT, WATHEAT)
+
+diff_heat <- paired_heat$WATHEAT - paired_heat$ENVHEAT
+shapiro.test(diff_heat)
+
+t.test(
+  paired_heat$ENVHEAT,
+  paired_heat$WATHEAT,
+  paired = TRUE,
+)
+
+mean_summary <- paired_cold |>
+  summarise(
+    Mean_ENVCOLD = mean(ENVCOLD, na.rm = TRUE),
+    SD_ENVCOLD = sd(ENVCOLD, na.rm = TRUE),
+    Mean_WATCOLD = mean(WATCOLD, na.rm = TRUE),
+    SD_WATCOLD = sd(WATCOLD, na.rm = TRUE),
+    n = n()
+  )
+
+mean_summary
+
+mean_summary <- paired_heat |>
+  summarise(
+    Mean_ENVHEAT = mean(ENVHEAT, na.rm = TRUE),
+    SD_ENVHEAT = sd(ENVHEAT, na.rm = TRUE),
+    Mean_WATHEAT = mean(WATHEAT, na.rm = TRUE),
+    SD_WATHEAT = sd(WATHEAT, na.rm = TRUE),
+    n = n()
+  )
+mean_summary
+
+#CV
+
+paired_cold <- HRV_metrics_baseline_cold |>
+  select(`Mass_(g)`, Treatment, CV) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = CV
+  ) |>
+  drop_na(ENVCOLD, WATCOLD)
+
+
+diff_cold <- paired_cold$WATCOLD - paired_cold$ENVCOLD
+shapiro.test(diff_cold)
+
+t.test(
+  paired_cold$ENVCOLD,
+  paired_cold$WATCOLD,
+  paired = TRUE
+)
+
+
+paired_heat <- HRV_metrics_baseline_heat |>
+  select(`Mass_(g)`, Treatment, CV) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = CV
+  ) |>
+  drop_na(ENVHEAT, WATHEAT)
+
+diff_heat <- paired_heat$WATHEAT - paired_heat$ENVHEAT
+shapiro.test(diff_heat)
+
+wilcox.test(
+  paired_heat$ENVHEAT,
+  paired_heat$WATHEAT,
+  paired = TRUE,
+)
+
+mean_summary <- paired_cold |>
+  summarise(
+    Mean_ENVCOLD = mean(ENVCOLD, na.rm = TRUE),
+    SD_ENVCOLD = sd(ENVCOLD, na.rm = TRUE),
+    mean_WATCOLD = mean(WATCOLD, na.rm = TRUE),
+    SD_WATCOLD = sd(WATCOLD, na.rm = TRUE),
+    n = n()
+  )
+
+mean_summary
+
+median_summary <- paired_heat |>
+  summarise(
+    Median_ENVHEAT = median(ENVHEAT, na.rm = TRUE),
+    IQR_ENVHEAT = IQR(ENVHEAT, na.rm = TRUE),
+    Median_WATHEAT = median(WATHEAT, na.rm = TRUE),
+    IQR_WATHEAT = IQR(WATHEAT, na.rm = TRUE),
+    n = n()
+  )
+median_summary
+
 
 HR_1 <- ggplot(
   data = HRV_metrics_baseline,
@@ -190,32 +287,6 @@ ggplot(
 
 ggplot(
   data = HRV_metrics_baseline,
-  aes(x = Treatment, y = pNN100, color = Block)
-) +
-  geom_boxplot(width = 0.5, outlier.shape = NA, alpha = 0.8, size = 0.5) +
-  geom_jitter(width = 0.1, size = 0.8, alpha = 0.4) +
-  scale_color_manual(
-    values = c(
-      "Cold" = "black",
-      "Heat" = "black"
-    )
-  ) +
-  stat_summary(
-    fun = mean,
-    geom = "point",
-    shape = 21,
-    fill = "darkred",
-    color = "darkred",
-    size = 1.5,
-    position = position_dodge(width = 0.4)
-  ) +
-  labs(x = "Baseline", y = "pNN100 (%)") +
-  theme_classic2() +
-  theme(legend.position = "Top")
-
-
-ggplot(
-  data = HRV_metrics_baseline,
   aes(x = Treatment, y = RMSSD, color = Block)
 ) +
   geom_boxplot(width = 0.5, outlier.shape = NA, alpha = 0.8, size = 0.5) +
@@ -268,69 +339,241 @@ ggplot(
   theme(legend.position = "Top")
 
 
-shapiro_test(HRV_metrics_baseline_heat$NNi)
-n <- HRV_metrics_baseline %>% group_by(Treatment) %>% shapiro_test(NNi)
-n
-
-mean_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment) %>%
-  summarise(mean_NN = mean(NNi), sd_NN = sd(NNi))
-mean_baseline
-
-t_test(HRV_metrics_baseline_cold, NNi ~ Treatment)
-t_test(HRV_metrics_baseline_heat, NNi ~ Treatment)
+paired_cold <- HRV_metrics_baseline_cold |>
+  select(`Mass_(g)`, Treatment, NNi) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = NNi
+  ) |>
+  drop_na(ENVCOLD, WATCOLD)
 
 
-shapiro_test(HRV_metrics_baseline_heat$pNN50)
-n <- HRV_metrics_baseline %>% group_by(Treatment) %>% shapiro_test(pNN50)
-n
+diff_cold <- paired_cold$WATCOLD - paired_cold$ENVCOLD
+shapiro.test(diff_cold)
 
 
-mean_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment) %>% dplyr::filter(Treatment %in%  c("ENVCOLD", "WATCOLD")) %>%
-  summarise(Median_pNN50 = mean(pNN50), sd_pNN50 = sd(pNN50))
-mean_baseline
-
-median_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment)  %>% dplyr::filter(Treatment %in% c("ENVHEAT","WATHEAT")) %>%
-  summarise(Median_pNN50 = median(pNN50), IQR_pNN50 = IQR(pNN50))
-median_baseline
+wilcox.test(
+  paired_cold$ENVCOLD,
+  paired_cold$WATCOLD,
+  paired = TRUE
+)
 
 
-t_test(HRV_metrics_baseline_cold, pNN50 ~ Treatment)
-wilcox_test(HRV_metrics_baseline_heat, pNN50 ~ Treatment)
+paired_heat <- HRV_metrics_baseline_heat |>
+  select(`Mass_(g)`, Treatment, NNi) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = NNi
+  ) |>
+  drop_na(ENVHEAT, WATHEAT)
+
+diff_heat <- paired_heat$WATHEAT - paired_heat$ENVHEAT
+shapiro.test(diff_heat)
+
+t.test(
+  paired_heat$ENVHEAT,
+  paired_heat$WATHEAT,
+  paired = TRUE,
+)
 
 
+median_summary <- paired_cold |>
+  summarise(
+    Median_ENVCOLD = median(ENVCOLD, na.rm = TRUE),
+    IQR_ENVCOLD = IQR(ENVCOLD, na.rm = TRUE),
+    Median_WATCOLD = median(WATCOLD, na.rm = TRUE),
+    IQR_WATCOLD = IQR(WATCOLD, na.rm = TRUE),
+    n = n()
+  )
 
-shapiro_test(HRV_metrics_baseline_heat$SDNN)
-n <- HRV_metrics_baseline %>% group_by(Treatment) %>% shapiro_test(SDNN)
-n
+median_summary
 
-
-mean_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment) %>% dplyr::filter(Treatment %in% c("ENVCOLD", "WATCOLD"))%>%
-  summarise(Mean_SDNN = mean(SDNN), sd_SDNN = sd(SDNN))
-mean_baseline
-
-median_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment) %>% dplyr::filter(Treatment %in% c("ENVHEAT", "WATHEAT"))%>%
-  summarise(Median_SDNN = median(SDNN), IQR_SDNN = IQR(SDNN))
-median_baseline
-
-
-t_test(HRV_metrics_baseline_cold, SDNN ~ Treatment)
-wilcox_test(HRV_metrics_baseline_heat, SDNN ~ Treatment)
-
-
-shapiro_test(HRV_metrics_baseline_heat$RMSSD)
-n <- HRV_metrics_baseline %>% group_by(Treatment) %>% shapiro_test(RMSSD)
-n
-
-median_baseline <- HRV_metrics_baseline %>%
-  group_by(Treatment) %>%
-  summarise(Median_RMSSD = median(RMSSD), IQR_RMSSD = IQR(RMSSD))
-median_baseline
+mean_summary <- paired_heat |>
+  summarise(
+    Mean_ENVHEAT = mean(ENVHEAT, na.rm = TRUE),
+    SD_ENVHEAT = sd(ENVHEAT, na.rm = TRUE),
+    Mean_WATHEAT = mean(WATHEAT, na.rm = TRUE),
+    SD_WATHEAT = sd(WATHEAT, na.rm = TRUE),
+    n = n()
+  )
+mean_summary
 
 
-wilcox_test(HRV_metrics_baseline_cold, RMSSD ~ Treatment)
-wilcox_test(HRV_metrics_baseline_heat, RMSSD ~ Treatment)
+paired_cold <- HRV_metrics_baseline_cold |>
+  select(`Mass_(g)`, Treatment, pNN50) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = pNN50
+  ) |>
+  drop_na(ENVCOLD, WATCOLD)
+
+
+diff_cold <- paired_cold$WATCOLD - paired_cold$ENVCOLD
+shapiro.test(diff_cold)
+
+
+t.test(
+  paired_cold$ENVCOLD,
+  paired_cold$WATCOLD,
+  paired = TRUE
+)
+
+
+paired_heat <- HRV_metrics_baseline_heat |>
+  select(`Mass_(g)`, Treatment, pNN50) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = pNN50
+  ) |>
+  drop_na(ENVHEAT, WATHEAT)
+
+diff_heat <- paired_heat$WATHEAT - paired_heat$ENVHEAT
+shapiro.test(diff_heat)
+
+t.test(
+  paired_heat$ENVHEAT,
+  paired_heat$WATHEAT,
+  paired = TRUE,
+)
+
+
+mean_summary <- paired_cold |>
+  summarise(
+    Mean_ENVCOLD = mean(ENVCOLD, na.rm = TRUE),
+    SD_ENVCOLD = sd(ENVCOLD, na.rm = TRUE),
+    Mean_WATCOLD = mean(WATCOLD, na.rm = TRUE),
+    SD_WATCOLD = sd(WATCOLD, na.rm = TRUE),
+    n = n()
+  )
+
+mean_summary
+
+mean_summary <- paired_heat |>
+  summarise(
+    Mean_ENVHEAT = mean(ENVHEAT, na.rm = TRUE),
+    SD_ENVHEAT = sd(ENVHEAT, na.rm = TRUE),
+    Mean_WATHEAT = mean(WATHEAT, na.rm = TRUE),
+    SD_WATHEAT = sd(WATHEAT, na.rm = TRUE),
+    n = n()
+  )
+mean_summary
+
+
+paired_cold <- HRV_metrics_baseline_cold |>
+  select(`Mass_(g)`, Treatment, SDNN) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = SDNN
+  ) |>
+  drop_na(ENVCOLD, WATCOLD)
+
+
+diff_cold <- paired_cold$WATCOLD - paired_cold$ENVCOLD
+shapiro.test(diff_cold)
+
+t.test(
+  paired_cold$ENVCOLD,
+  paired_cold$WATCOLD,
+  paired = TRUE
+)
+
+
+paired_heat <- HRV_metrics_baseline_heat |>
+  select(`Mass_(g)`, Treatment, SDNN) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = SDNN
+  ) |>
+  drop_na(ENVHEAT, WATHEAT)
+
+diff_heat <- paired_heat$WATHEAT - paired_heat$ENVHEAT
+shapiro.test(diff_heat)
+
+wilcox.test(
+  paired_heat$ENVHEAT,
+  paired_heat$WATHEAT,
+  paired = TRUE,
+)
+
+
+mean_summary <- paired_cold |>
+  summarise(
+    Mean_ENVCOLD = mean(ENVCOLD, na.rm = TRUE),
+    SD_ENVCOLD = sd(ENVCOLD, na.rm = TRUE),
+    Mean_WATCOLD = mean(WATCOLD, na.rm = TRUE),
+    SD_WATCOLD = sd(WATCOLD, na.rm = TRUE),
+    n = n()
+  )
+
+mean_summary
+
+
+median_summary <- paired_heat |>
+  summarise(
+    Median_ENVHEAT = median(ENVHEAT, na.rm = TRUE),
+    IQR_ENVHEAT = IQR(ENVHEAT, na.rm = TRUE),
+    Median_WATHEAT = median(WATHEAT, na.rm = TRUE),
+    IQR_WATHEAT = IQR(WATHEAT, na.rm = TRUE),
+    n = n()
+  )
+median_summary
+
+paired_cold <- HRV_metrics_baseline_cold |>
+  select(`Mass_(g)`, Treatment, RMSSD) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = RMSSD
+  ) |>
+  drop_na(ENVCOLD, WATCOLD)
+
+
+diff_cold <- paired_cold$WATCOLD - paired_cold$ENVCOLD
+shapiro.test(diff_cold)
+
+
+t.test(
+  paired_cold$ENVCOLD,
+  paired_cold$WATCOLD,
+  paired = TRUE
+)
+
+
+paired_heat <- HRV_metrics_baseline_heat |>
+  select(`Mass_(g)`, Treatment, RMSSD) |>
+  pivot_wider(
+    names_from = Treatment,
+    values_from = RMSSD
+  ) |>
+  drop_na(ENVHEAT, WATHEAT)
+
+diff_heat <- paired_heat$WATHEAT - paired_heat$ENVHEAT
+shapiro.test(diff_heat)
+
+wilcox.test(
+  paired_heat$ENVHEAT,
+  paired_heat$WATHEAT,
+  paired = TRUE
+)
+
+
+mean_summary <- paired_cold |>
+  summarise(
+    Mean_ENVCOLD = mean(ENVCOLD, na.rm = TRUE),
+    SD_ENVCOLD = sd(ENVCOLD, na.rm = TRUE),
+    Mean_WATCOLD = mean(WATCOLD, na.rm = TRUE),
+    SD_WATCOLD = sd(WATCOLD, na.rm = TRUE),
+    n = n()
+  )
+
+mean_summary
+
+
+median_summary <- paired_heat |>
+  summarise(
+    Median_ENVHEAT = median(ENVHEAT, na.rm = TRUE),
+    IQR_ENVHEAT = IQR(ENVHEAT, na.rm = TRUE),
+    Median_WATHEAT = median(WATHEAT, na.rm = TRUE),
+    IQR_WATHEAT = IQR(WATHEAT, na.rm = TRUE),
+    n = n()
+  )
+median_summary
